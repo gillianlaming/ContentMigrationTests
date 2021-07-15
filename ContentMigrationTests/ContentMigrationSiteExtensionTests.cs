b8d698fba2bca680173a13d6c91f5b0e95da231d
Gillian Laming <glaming@microsoft.com>
2021-07-15 16:42:33 -0700

    Add project files.

---
+++ ContentMigrationTests/ContentMigrationSiteExtensionTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Web.Hosting;
+using Microsoft.Web.Hosting.Administration;
+using Microsoft.Web.Hosting.Administration.Data.Model;
+using System;
+
+namespace ContentMigrationTests
+{
+    [TestClass]
+    public class ContentMigrationSiteExtensionTests
+    {
+        private ContentMigration _contentMigration;
+
+        public ContentMigrationSiteExtensionTests()
+        {
+            Guid id = new Guid();
+            //need to mock a website
+            //mock irepositorycontext
+            _contentMigration = new ContentMigration(id, )
+        }
+
+        [TestMethod]
+        public void TestInitialCallToExtension()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestPollingStatusFromExtension()
+        {
+
+        }
+    }
+}
---
+++ ContentMigrationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ContentMigrationTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ContentMigrationTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b1ac08af-4d66-4e1b-b8c4-41a09487fc00")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
