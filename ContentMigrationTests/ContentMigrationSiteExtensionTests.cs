669ba235cdc9d3a5b7d2479a94fd9e63cb65acbd
Gillian Laming <glaming@microsoft.com>
2021-07-16 11:06:50 -0700

    first test is passing

--- ContentMigrationTests/ContentMigrationSiteExtensionTests.cs
+++ ContentMigrationTests/ContentMigrationSiteExtensionTests.cs
@@ -1,10 +1,13 @@
-﻿using ImportExportService;
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using ImportExportService;
 using Microsoft.Web.Hosting;
 using Microsoft.Web.Hosting.Administration;
+using Microsoft.Web.Hosting.Administration.Common;
 using Microsoft.Web.Hosting.Administration.Data;
 using Microsoft.Web.Hosting.Administration.Data.Model;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using Moq;
 
 namespace ContentMigrationTests
@@ -12,11 +15,13 @@ namespace ContentMigrationTests
     [TestClass]
     public class ContentMigrationSiteExtensionTests
     {
-        private ContentMigration _contentMigration;
+        private readonly ContentMigration _contentMigration;
 
         private readonly Mock<WebSite> _webSiteMock = new Mock<WebSite>();
         private readonly Mock<IRepositoryContext> _dbMock = new Mock<IRepositoryContext>();
-        private readonly Mock<ContentMigrationCaller> _callerMock = new Mock<ContentMigrationCaller>();
+        private readonly Mock<IContentMigrationCaller> _callerMock = new Mock<IContentMigrationCaller>();
+        private readonly HttpStatusCode _expectedStatusResponse = HttpStatusCode.OK;
+
         public ContentMigrationSiteExtensionTests()
         {
             //Assign dummy values when needed
@@ -26,13 +31,26 @@ namespace ContentMigrationTests
             int notRevertMigration = 0;
             //why is content migration status not loading??
 
-            _contentMigration = new ContentMigration(id, _webSiteMock.Object.Id, destinationPath, 0, _dbMock.Object, _webSiteMock.Object, revertFlag: notRevertMigration);
+            _contentMigration = new ContentMigration(); //new ContentMigration(id, _webSiteMock.Object.Id, destinationPath, 0, _dbMock.Object, _webSiteMock.Object,notRevertMigration);
         }
 
         [TestMethod]
         public void TestInitialCallToExtension()
         {
+            Guid id = new Guid();
+            MigrationRequest requestBody = new MigrationRequest()
+            {
+                MigrationId = id.ToString(),
+                DestinationPath = "volume-0-default", 
+                RuntimeSiteName = "glaming3site",
+                Round = 1
+            };
+            HttpResponseMessage responseMessage = new HttpResponseMessage(); 
+            _callerMock.Setup(x => x.MakePostRequestToScmSite(_contentMigration, requestBody)).Returns(responseMessage);
 
+            var response = _callerMock.Object.MakePostRequestToScmSite(_contentMigration, requestBody);
+
+            Assert.AreEqual(expected: _expectedStatusResponse, actual: response.StatusCode);
         }
 
         [TestMethod]
